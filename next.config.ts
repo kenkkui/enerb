9dfb90d86918edcfd68420fc4bff91a34c64cab6
kenkkui <thifirstime@gmail.com>
2025-04-08 23:38:03 +0800

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // enables static export
+  images: { unoptimized: true }, // if you're using next/image
+  assetPrefix: "/ener-b", // match GitHub repo name
+  basePath: "/kenkkui",
 };
 
 export default nextConfig;
